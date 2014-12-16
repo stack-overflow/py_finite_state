e44dc4d68845845f601803f31e10833a24cdb27c
Mateusz Zych <mte.zych@gmail.com>
2014-12-17 00:55:33 +0100

    Add check for empty sequence or regex.

--- prosite_app.py
+++ prosite_app.py
@@ -12,35 +12,41 @@ if __name__ == '__main__':
 	sequence = input("Sequence: ")
 	regex = input("Regular expression: ")
 
-	prositeMatcher = prosite_matcher.PrositeMatcher()
-	prositeMatcher.compile(regex)
-	matches, ranges = prositeMatcher.get_matches(sequence)
+	if sequence != None and sequence != "" and regex != None and regex != "":
 
-	print("Found patterns: ", end="")
+		prositeMatcher = prosite_matcher.PrositeMatcher()
+		prositeMatcher.compile(regex)
+		matches, ranges = prositeMatcher.get_matches(sequence)
 
-	if (len(matches) > 0):
+		print("Found patterns: ", end="")
 
-		print(sequence[ 0 : ranges[0][0] ], end="")
+		if (len(matches) > 0):
 
-		for i in range(0, len(matches)):
+			print(sequence[ 0 : ranges[0][0] ], end="")
 
-			print("\033[91m", end="")
-			print(sequence[ ranges[i][0] : ranges[i][1] ], end="")
-			print("\033[0m", end="")
+			for i in range(0, len(matches)):
 
-			if (i < len(matches) - 1):
-				print(sequence[ ranges[i][1] : ranges[i + 1][0] ], end="")
+				print("\033[91m", end="")
+				print(sequence[ ranges[i][0] : ranges[i][1] ], end="")
+				print("\033[0m", end="")
 
-		print(sequence[ ranges[len(ranges) - 1][1] : len(sequence)])
+				if (i < len(matches) - 1):
+					print(sequence[ ranges[i][1] : ranges[i + 1][0] ], end="")
+
+			print(sequence[ ranges[len(ranges) - 1][1] : len(sequence)])
+
+		else:
+
+			print(sequence)
+
+		print("")
+
+		for elem in list(zip(matches, ranges)):
+			print(elem[0], end=" ")
+			print(elem[1])
+
+		print("")
 
 	else:
 
-		print(sequence)
-
-	print("")
-
-	for elem in list(zip(matches, ranges)):
-		print(elem[0], end=" ")
-		print(elem[1])
-
-	print("")
+		print("Sequence and regular expression can't be empty.")
